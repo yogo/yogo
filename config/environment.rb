4348ba9b9a63b037241734ebb720b6ef0824dd34
Pol Llovet <pol.llovet@gmail.com>
2009-07-14 10:43:28 -0600

    moved the database.yml file to example modified gem names

--- config/environment.rb
+++ config/environment.rb
@@ -21,7 +21,8 @@ Rails::Initializer.run do |config|
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem "dm-core"
   config.gem "rails_datamapper", :version => '0.10.0'
-  config.gem "dm-persevere-adapter", :lib => "persevere_adapter"
+  config.gem "irjudson-dm-persevere-adapter", :lib => "persevere_adapter"
+  config.gem "irjudson-persevere", :lib => "persevere"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
