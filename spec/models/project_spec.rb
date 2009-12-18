5ab1ac99b6be66f240a42a45c0f99df1f9a21bbc
Pol Llovet <pol.llovet@gmail.com>
2009-12-18 12:43:36 -0700

    change lambda counts to static variables
    lambda counts don't work with dm collections i guess.

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -2,16 +2,16 @@ require File.expand_path(File.join(File.dirname(__FILE__), '..', 'spec_helper'))
 
 describe "A Project" do
   it "should not be created without a name" do
-    lambda do
-      p = Project.new
-      p.should_not be_valid   
-      p.save
-    end.should_not change(Project.all,:count)
+    count = Project.all.count
+    p = Project.new
+    p.should_not be_valid   
+    p.save
+    count.should == Project.all.count
   end
 
   it "should be created with a name" do
-    lambda do
-      p = Project.create(:name => "Test Project")
-    end.should change(Project.all,:count).by(1)
+    count = Project.all.count
+    p = Project.create(:name => "Test Project")
+    count.should == Project.all.count - 1
   end
 end
