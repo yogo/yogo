b952d3f4019a9b4fae583c34c5ec111265d32382
Robbie Lamb <robbie.lamb@gmail.com>
2010-01-05 15:34:28 -0700

    A very basic pagination helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,22 @@ module ApplicationHelper
       link_to(name, url, html_options) +
     "</li>"
   end
+  
+  def pagination_links(collection, cur_page = 1, per_page = 5)
+
+    total_pages = collection.model.page_count(:per_page => per_page)
+    current_page = cur_page.nil? ? 1 : cur_page.to_i
+
+    output = ""
+
+    if current_page > 1
+      output += link_to("<- Previous", "?page=#{current_page-1}")
+    end
+    # output += ' PAGINATION '
+    if current_page < total_pages
+      output += link_to(" Next ->", "?page=#{current_page+1}")
+    end
+
+    output
+  end
 end
