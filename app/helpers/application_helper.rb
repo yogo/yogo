fff12bac0ca7f74464bc0ad3ba5af37221e6ba64
irjudson <irjudson@gmail.com>
2010-01-05 10:27:13 -0700

    Updated layout and resolving issue #7660. Work to be done includes:
    1) ensuring compliance with look and feel guidelines
    2) Finding a suitable image header (if we so choose).

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,11 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def add_menu_item(name, url = {}, html_options = {})
+    css_class = ''
+    css_class = 'highlighted-menu-item' if request.url.include?(h(url))
+      
+    "<li class='#{css_class}'>" +
+      link_to(name, url, html_options) +
+    "</li>"
+  end
 end
