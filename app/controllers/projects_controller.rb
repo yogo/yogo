7a4a67476ce40c9293530163cc9df64e0b0ea37c
Robbie Lamb <robbie.lamb@gmail.com>
2010-01-05 10:41:00 -0700

    Beginning of pagination in projects controller.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,7 +1,7 @@
 class ProjectsController < ApplicationController
 
   def index
-    @projects = Project.all
+    @projects = Project.paginate(:page => params[:page], :per_page => 5)
   end
   
   def new
