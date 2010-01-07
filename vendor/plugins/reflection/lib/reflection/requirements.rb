5a0e036b1b7a756eaea5e812271e911710846771
jaeger.ca <jaeger.ca@gmail.com>
2010-01-07 13:43:49 -0700

    Fixed JSON Behavior

--- vendor/plugins/reflection/lib/reflection/requirements.rb
+++ vendor/plugins/reflection/lib/reflection/requirements.rb
@@ -2,6 +2,7 @@ require 'reflection/databases/sqlite3'
 require 'reflection/databases/persevere'
 require 'reflection/databases/my_sql'
 require 'reflection/databases/postgres'
+#require 'reflection/custom_reflect'
 
 require 'reflection/attribute'
 require 'reflection/csv'
