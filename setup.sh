8bd4aa21412bc7a6755283b0e7a1e5886e97afdd
Ryan Heimbuch <rheimbuch@gmail.com>
2010-01-15 11:58:49 -0700

    Set NO_PERSEVERE during setup.

--- setup.sh
+++ setup.sh
@@ -6,5 +6,5 @@ git submodule init && \
 git submodule update && \
 $RUBY gem install bundler && \
 $RUBY gem bundle && \
-$RUBY rake yogo:setup && \
-$RUBY rake db:seed
+$RUBY rake yogo:setup NO_PERSEVERE=true && \
+$RUBY rake db:seed NO_PERSEVERE=true
