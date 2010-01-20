a36f30e15f97edc998d17a87dfd33b4c0d0122f8
Robbie Lamb <robbie.lamb@gmail.com>
2010-01-20 11:17:26 -0700

    setup.sh can now be run from anywhere in the filesystem.

--- setup.sh
+++ setup.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+pushd `dirname $0`
+
 RUBY="ruby -X-C -S"
 echo "Configuring fresh yogo checkout for development..."
 git submodule init && \
 git submodule update && \
 $RUBY gem install bundler && \
 $RUBY gem bundle && \
-$RUBY rake yogo:setup NO_PERSEVERE=true && \
-$RUBY rake db:seed NO_PERSEVERE=true
+$RUBY bin/rake yogo:setup NO_PERSEVERE=true && \
+$RUBY bin/rake db:seed NO_PERSEVERE=true
+
+popd
