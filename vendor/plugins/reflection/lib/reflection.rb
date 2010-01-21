aa2a0766f35f805072869af428838e553a0c6658
irjudson <irjudson@gmail.com>
2010-01-21 10:55:50 -0700

    Remove reflection plugin (replaced with vendor/gems/dm-reflection)

--- vendor/plugins/reflection/lib/reflection.rb
+++
@@ -1,8 +0,0 @@
-require 'reflection/reflection'
-require 'reflection/parse_types'
-require 'reflection/attribute'
-require 'reflection/csv'
-require 'reflection/adapters/sqlite3_adapter'
-require 'reflection/adapters/persevere_adapter'
-require 'reflection/adapters/mysql_adapter'
-require 'reflection/adapters/postgres_adapter'
