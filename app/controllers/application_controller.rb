e3d6ca1855a052103aa7073cda56607ecd0e67f5
Robbie Lamb <robbie.lamb@gmail.com>
2010-01-22 12:45:06 -0700

    Created a setting to determining local access only or globally.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,22 @@ class ApplicationController < ActionController::Base
   include Yogo::AuthenticatedSystem
   include Yogo::AuthorizationSystem
   
+  before_filter :check_local_only
+  
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
   # Scrub sensitive parameters from your log
   filter_parameter_logging :password
+  
+  private
+  
+  def check_local_only
+
+    if Yogo::Settings[:local_only] && !request.env["REMOTE_ADDR"].match("127.0.0.1")
+      # Raise a 403 exception or perhaps just redirect.
+      render(:text => 'This resource is forbidden.', :status => 403) and return
+    end
+  end
+  
 end
