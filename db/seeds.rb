7129fd9d4783837083882f0f59bce52141e36e6f
Sean Cleveland <seancleveland@sean-clevelands-macbook-pro.local>
2010-01-28 11:53:50 -0700

    Commented out User references in the seeds file that was preventing a successful db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,31 +6,31 @@
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Major.create(:name => 'Daley', :city => cities.first)
 Dir[File.join(RAILS_ROOT, "app", "models", "**", "*.rb")].each{|f| require f}
-Dir[File.join(RAILS_ROOT, "vendor", "plugins", "*", "app", "models", "**", "*.rb")].each{|f| require f}
+# Dir[File.join(RAILS_ROOT, "vendor", "plugins", "*", "app", "models", "**", "*.rb")].each{|f| require f}
 DataMapper.auto_migrate!
 
-begin # Yogo  initialization
-  puts "Creating default user and admin group."
-  User.create(:login => "default_user",
-              :first_name            => "Default",
-              :last_name             => "User",
-              :email                 => "default_user@example.com",
-              :password              => "p@55w0rd",
-              :password_confirmation => "p@55w0rd"
-              )
-          
-  user = User.first(:login => 'default_user')
-
-  default_group = Yogo::Group.create(:name => 'default')
-  
-  Yogo::Group.create(:name => Yogo::Settings[:anonymous_user_group], :foreign_id => -1)
-
-  Yogo::Group.create(:parent      => default_group, 
-                          :name        => 'admin',
-                          :description => 'System Administrators.',
-                          :sysadmin    => true)
-                        
-  Yogo::Membership.create(:user => user, 
-                    :group => Yogo::Group.first(:name => 'admin'))
-  puts "Done"
-end
+# begin # Yogo  initialization
+#   puts "Creating default user and admin group."
+#   User.create(:login => "default_user",
+#               :first_name            => "Default",
+#               :last_name             => "User",
+#               :email                 => "default_user@example.com",
+#               :password              => "p@55w0rd",
+#               :password_confirmation => "p@55w0rd"
+#               )
+#           
+#   user = User.first(:login => 'default_user')
+# 
+#   default_group = Yogo::Group.create(:name => 'default')
+#   
+#   Yogo::Group.create(:name => Yogo::Settings[:anonymous_user_group], :foreign_id => -1)
+# 
+#   Yogo::Group.create(:parent      => default_group, 
+#                           :name        => 'admin',
+#                           :description => 'System Administrators.',
+#                           :sysadmin    => true)
+#                         
+#   Yogo::Membership.create(:user => user, 
+#                     :group => Yogo::Group.first(:name => 'admin'))
+#   puts "Done"
+# end
