cc4aa4fae38936a998c7a61a4cd85cc2a4276593
jaeger.ca <jaeger.ca@gmail.com>
2010-02-16 15:53:28 -0700

    Added auto_migration to nav models

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,3 +15,7 @@
 # 
 Dir[File.join(RAILS_ROOT, "app", "models", "**", "*.rb")].each{|f| require f}
 DataMapper.auto_migrate!
+NavModel.auto_migrate!
+NavAttribute.auto_migrate!
+NavDisplayValue.auto_migrate!
+NavDatabaseValue.auto_migrate!
