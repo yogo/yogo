1d5653e85017baf0aabd7f33bcd418236fc500fa
Robbie Lamb <robbie.lamb@gmail.com>
2010-02-22 09:48:18 -0700

    Breadcrumbs have been made sane.

--- config/initializers/breadcrumbs.rb
+++ config/initializers/breadcrumbs.rb
@@ -1,16 +1,22 @@
 Breadcrumb.configure do
-  crumb :root, "Projects", :projects_url
-  crumb :project, '#{Project.get(params[:id]).name}', :project_url, :id
-  crumb :project_embedded, '#{Project.get(params[:project_id]).name}', 'project_url(@project)'
-  crumb :project_models, 'Models', :project_yogo_models_url, :project_id
-  crumb :project_model, 'Model: #{params[:id]}', :project_yogo_model_url, :params => :id
-  #crumb :project_data, "Data", :project_yogo_data_url, :params => :project_id
+  # The second parameter here is never evaled, so the string we use must be set.
+  crumb :root,                "Projects",         :projects_path
+  crumb :project,             '#{@project.name}', :project_path,                 :project
+  crumb :project_search,      'Search: #{params[:search_term]}', :search_project_path
+  crumb :project_models,      'Models',           :project_yogo_models_path,     :project
+  crumb :project_model,       'Model: #{params[:id]}', :project_yogo_model_path, :project
+  crumb :project_data_index,  "Data",             :project_yogo_data_index_path, '@project.id', '@model.name.demodulize'
+  crumb :project_data,        '#{@item.id}',      :project_yogo_data_path,       '@project.id', '@model.name.demodulize', '@item.id'
+  crumb :project_data_search, 'Search: #{params[:search_term]}', :search_project_path
   
-  trail :projects, :index, [:root]
-  trail :projects, [:show, :edit], [:root, :project]
-  trail :yogo_models, :index, [:root, :project_embedded, :project_models ]
-  trail :yogo_models, [:show, :edit], [:root, :project_embedded, :project_model ]
-  #trail :yogo_data, [:show, :edit], [:root, :project, :project_data ]
+  trail :projects,    :index,         [:root]
+  trail :projects,    [:show, :edit], [:root, :project]
+  trail :projects,    :search,        [:root, :project_search]
+  trail :yogo_models, :index,         [:root, :project, :project_models ]
+  trail :yogo_models, [:show, :edit], [:root, :project, :project_model ]
+  trail :yogo_data,   :index,         [ :root, :project, :project_data_index ]
+  trail :yogo_data,   [:show, :edit], [:root, :project, :project_data_index, :project_data ]
+  trail :yogo_data,   :search,        [:root, :project, :project_data_index, :project_data_search]
 
   delimit_with " > "
   dont_link_last_crumb
