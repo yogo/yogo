d0adb9de1d2513e2e25e2a312de19f4c8d2eabb5
Robbie Lamb <robbie.lamb@gmail.com>
2010-02-24 12:10:15 -0700

    Changed from joining a queryscope to unioning them.

--- app/controllers/yogo_data_controller.rb
+++ app/controllers/yogo_data_controller.rb
@@ -149,10 +149,10 @@ class YogoDataController < ApplicationController
         if @query_scope.nil?
           @query_scope = @model.all(attribute.to_sym => condition)
         else
-          @query_scope = @query_scope | @model.all(attribute.to_sym => condition)
+          @query_scope = @query_scope + @model.all(attribute.to_sym => condition)
         end
       end
-      # debugger
+      
       @histogram = Yogo::Navigation.values(@query_scope, @attribute_name.to_sym)
     end
 
