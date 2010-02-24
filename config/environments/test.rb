f9150b3e49727180a2df20804a5c715d4d09e6fd
Robbie Lamb <robbie.lamb@gmail.com>
2010-02-24 10:06:38 -0700

    Started moving to bundler 0.9

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -6,7 +6,6 @@
 # FILE: test.rb
 # 
 #
-Bundler.require_env RAILS_ENV
 # Settings specified here will take precedence over those in config/environment.rb
 
 # The test environment is used exclusively to run your application's
