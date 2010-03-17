63f400d87b0ab179e656a812d23c0f8a69d2a29f
Robbie Lamb <robbie.lamb@gmail.com>
2010-03-17 15:36:25 -0600

    yogo_id should be integrated and probably working.

--- lib/yogo/reflection.rb
+++ lib/yogo/reflection.rb
@@ -9,11 +9,13 @@ module Yogo
           results = super
           results.each do |result|
             if result[:name].eql?('id')
-              result[:name] == 'yogo_id'
-              result[:field] == 'id'
+              result[:name] = 'yogo_id'
+              result[:field] = 'id'
+            elsif result[:name].match(/^yogo___/)
+              result[:field] == result[:name]
+              result[:name] = result[:name].gsub(/^yogo___/, '')
             end
           end
-          puts 'my get_props called'
           results
         end
       end
@@ -26,13 +28,10 @@ module DataMapper
   module Adapters
     extendable do
       def const_added(const_name)
-        puts 'Calling my duck punching'
         super
-        puts "blah: #{const_name}"
         if Yogo::Reflection.const_defined?(const_name)
-          puts 'including my duck punch'
           adapter = const_get(const_name)
-          adapter.send(:include, DataMapper::YogoFileAdapterExtensions.const_get(const_name))
+          adapter.send(:include, Yogo::Reflection.const_get(const_name))
         end
       end
     end
