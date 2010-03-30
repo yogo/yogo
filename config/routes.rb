ec2974ab36709f90bba65018a08dfb2fa813b19f
Libbey White <currant@gmail.com>
2010-03-30 10:50:14 -0600

    Adding mockup to routes

--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,7 @@ ActionController::Routing::Routes.draw do |map|
   end
   map.resources :yogo_settings
   map.resources :tutorial
+  map.resources :mockup
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
   map.root :controller => "welcome"
 end
