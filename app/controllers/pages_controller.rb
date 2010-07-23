4c68212cf4a605e7e81866c4d6c18bb1dc1aef05
irjudson <irjudson@gmail.com>
2010-07-23 10:35:49 -0600

    Files added for VOEIS.

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,13 @@
+# app/controllers/pages_controller.rb
+class PagesController < ApplicationController
+
+  rescue_from ActionView::MissingTemplate, :with => :invalid_page
+
+  def show
+    render params[:id]
+  end
+
+  def invalid_page
+    redirect_to root_path
+  end
+end
