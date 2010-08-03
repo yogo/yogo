27491880830c6df65f0772e3a55f0f2c38f85655
Robbie Lamb <robbie.lamb@gmail.com>
2010-08-03 14:34:51 -0600

    Updating the projects controller

--- app/controllers/yogo/projects_controller.rb
+++ app/controllers/yogo/projects_controller.rb
@@ -8,8 +8,14 @@
 # and additionally: upload of CSV files, an example project and rereflection
 # of the yogo repository.
 
-class Yogo::ProjectsController < ApplicationController
+class Yogo::ProjectsController < InheritedResources::Base
+  respond_to :html, :json
   
+  defaults :resource_class => Yogo::Project,
+           :collection_name => 'projects',
+           :instance_name => 'project'
+  
+  ##
   # Show all the projects
   #
   # @example 
@@ -21,323 +27,29 @@ class Yogo::ProjectsController < ApplicationController
   #
   # @api public
   def index
-    @projects = Yogo::Project.paginate(:page => params[:page], :per_page => 5)
-    
-     respond_to do |format|
-        if @projects.empty?
-          @no_search = true
-          @no_menu   = true 
-          format.html { render('no_projects') }
-        else
-          format.html 
-        end
-      end
-  end
-
-  # Find a project or projects and show the result
-  #
-  # @example 
-  #   get /projects/search?q=search-term
-  #
-  # @return [Model] searches for data across all project all models all content of models
-  #
-  # @author Yogo Team
-  #
-  # @api public
-  def search
-    @search_scope = params[:search_scope]
-    @search_term = params[:search_term]
-    if @search_scope == 'everywhere' || params[:model_name].blank?
-      @projects = Project.available.search(@search_term)
-
-      @proj_models = []
-      Project.available.each do |project|
-        @proj_models << [project, project.search_models(@search_term).flatten ]
-      end
-
-      @proj_models_data = []
-      Project.available.each do |project|
-        project.models.each do |model|
-          count = model.search(@search_term).count
-          @proj_models_data << [project, model, count] if count > 0
-        end
-      end
-
-      respond_to do |format|
-        format.html {
-          if @proj_models_data.length == 1
-            redirect_to(search_project_yogo_data_url(@proj_models_data[0][0], 
-                                                     @proj_models_data[0][1], 
-                                                     :search_term => @search_term))
-          end
-        }
-      end
-      
-    else
-      project = Project.get(params[:project_id])
-      model = project.get_model(params[:model_name])
-      respond_to do |format|
-        format.html { 
-          redirect_to search_project_yogo_data_url(project, model, :search_term => @search_term) 
-        }
-      end
-    end
-
-  end
-
-  ##
-  # Shows a project
-  #
-  # @example 
-  #   get /projects/1 # Returnes project with an id of 1
-  #
-  # @return [Object] returns a web page displaying a project
-  #
-  # @author Yogo Team
-  #
-  # @api public
-  def show
-    @project = Yogo::Project.get(params[:id])
-    
-    if !Yogo::Setting[:local_only] && @project.is_private?
-      raise AuthenticationError if !logged_in?
-      raise AuthorizationError  if !current_user.is_in_project?(@project)
-    end
-    
-    @data_collections = @project.data_collections
-    @sidebar = true
-    
-    respond_to do |format|
-      format.html
-    end
-  end
-
-  ##
-  # Returns a form for creating a new project
-  #
-  # @example 
-  #   get /projects/new
-  #
-  # @return [Object] returns an empty project
-  #
-  # @author Yogo Team
-  #
-  # @api public
-  def new
-    @project = Yogo::Project.new
-    
-    respond_to do |format|
-      format.html 
-    end
-    
-  end
-
-  ##
-  # Creates a new project based on the attributes
-  #
-  # @example 
-  #   post /projects
-  #
-  # @param [Hash] params
-  # @option params [Hash] :project this is the attributes of a project
-  #
-  # @return if the project saves correctly it redirects to show project 
-  #  else it redirects to new project page
-  #
-  # @author Yogo Team
-  #
-  # @api public
-  def create
-    if !Yogo::Setting[:local_only]
-      raise AuthenticationError  if !logged_in? 
-      raise AuthorizationError if !current_user.has_permission?(:create_projects)
-    end
-
-    
-    @project = Yogo::Project.new(params[:yogo_project])
-    if @project.save
-      flash[:notice] = "Project \"#{@project.name}\" has been created."
-      redirect_to yogo_project_url(@project)
-    else
-      flash.now[:error] = "Project could not be created."
-      render :action => :new
-    end
-  end
-
-  ##
-  # load project for editing
-  #
-  # @example 
-  #  get /projects/1/edit # edits project with an id of 1
-  #
-  # @param [Hash] params
-  # @option params [String]:id
-  #
-  # @return [Object] returns a project
-  #
-  # @author Yogo Team
-  #
-  # @api public
-  def edit
-    @project = Yogo::Project.get(params[:id])
-    
-    if !Yogo::Setting[:local_only]
-      raise AuthenticationError unless logged_in? 
-      raise AuthorizationError  unless @project.groups.users.empty? || current_user.has_permission?(:edit_project,@project)
-    end
-    
-    respond_to do |format|
-      format.html
-    end
-  end
-
-  ##
-  # Updates project with new values
-  #
-  # @example 
-  #   put /projects/1
-  #
-  # @param [Hash] params
-  # @option params [String]:id
-  # @option params [Hash] :project
-  #
-  # @return if the project saves correctly it redirects to show project 
-  #  else it redirects to edit project page
-  #
-  # @author Yogo Team
-  #
-  # @api public
-  def update
-    @project = Yogo::Project.get(params[:id])
-    
-    if !Yogo::Setting[:local_only]
-      raise AuthenticationError unless logged_in? 
-      raise AuthorizationError  unless current_user.has_permission?(:edit_project,@project)
-    end
-    
-    params[:project].delete(:name) if params.has_key?(:project)
-    @project.attributes = params[:yogo_project]
-    if @project.save
-      flash[:notice] = "Project \"#{@project.name}\" has been updated."
-      redirect_to yogo_projects_url
-    else
-      flash[:error] = "Project could not be updated."
-      render( :action => :edit )
-    end
-  end
-
-  ##
-  # deletes a project
-  #
-  # @example 
-  #   destroy /projects/1
-  #
-  # @param [Hash] params
-  # @option params [String]:id
-  #
-  # @return no matter what it redirects to
-  #  project index page
-  #
-  # @author Yogo Team
-  #
-  # @api public
-  def destroy
-    @project = Yogo::Project.get(params[:id])
-    
-    if !Yogo::Setting[:local_only]
-      raise AuthenticationError unless logged_in? 
-      raise AuthorizationError  unless current_user.has_permission?(:edit_project,@project)
-    end
-    
-    if @project.destroy
-      flash[:notice] = "Project \"#{@project.name}\" has been destroyed."
-    else
-      flash[:error] = "Project \"#{@project.name}\" could not be destroyed."
-    end
-    redirect_to projects_url
-  end
-
-  # Create a new dataset on the project with a CSV file
-  #
-  # @example 
-  #  post /projects/upload/1 # with a CSV file
-  #
-  # @param [Hash] params
-  # @option params [String]:id
-  # @option params [Hash] :upload
-  #
-  # @return [] always redirects to showproject
-  #
-  # @author Yogo Team
-  #
-  # @api public
-  def upload
-    @project = Project.get(params[:id])
-    
-    if !Yogo::Setting[:local_only]
-      raise AuthenticationError unless logged_in? 
-      raise AuthorizationError  unless current_user.has_permission?(:edit_project,@project)
-    end
-    
-    if !params[:upload].nil?
-      datafile = params[:upload]['datafile']
-      
-      if !['text/csv', 'text/comma-separated-values', 'application/vnd.ms-excel',
-            'application/octet-stream','application/csv'].include?(datafile.content_type)
-        flash[:error] = "File type #{datafile.content_type} not allowed"
-        #redirect_to project_url(@project)
+    super do |format|
+      if collection.empty?
+        @no_search = true
+        @no_menu   = true 
+        format.html { render('no_projects') }
       else
-        class_name = File.basename(datafile.original_filename, ".csv").singularize.camelcase
-
-        errors =  @project.process_csv(datafile.path, class_name)
-
-        if errors.empty?
-          flash[:notice]  = "Spreadsheet imported succesfully."
-        else
-          flash[:error] = errors.join("\n")
-        end
+        format.html 
       end
-      
-    else
-       flash[:error] = "Spreadsheet import error, please try the upload again."
     end
-    
-    redirect_to project_url(@project)
   end
+  
+  protected
 
-  # loads example project and models in Yogo
-  #
-  # @example 
-  #   get /projects/loadexample
-  #
-  # @return redirects to project index page
-  #
-  # @todo Figure out how this should act when in server mode.
-  # 
-  # @author Yogo Team
-  #
-  # @api public
-  def loadexample
-    # Load the cercal db from CSV
-
-    if !Yogo::Setting[:local_only] && (!logged_in?)
-      raise AuthenticationError
-    end
-
-    @project = Yogo::Project.create(:name => "Cricket Cercal System DB")
-    if @project.valid?
-      errors = @project.process_csv(Rails.root.join("dist", "example_data", "cercaldb", "cells.csv"), "Cell")
-      if errors.empty?
-        flash[:notice]  = "Example Project imported succesfully."
-      else
-        flash[:error] = errors.join("\n")
-      end
-      Yogo::Setting[:example_project_loaded] = true
-      redirect_to project_url(@project)
-    else
-      flash[:error] = "Example Project could not be created, so was not loaded."
-      redirect_to root_url
-    end
+  def resource
+    @project ||= collection.get(params[:id])
+  end
+  
+  def collection
+    @projects ||= resource_class.all.paginate(:page => params[:page], :per_page => 5)
+  end
+  
+  def resource_class
+    Yogo::Project
   end
   
 end
