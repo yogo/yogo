b7a7085e1327edf189955082243dbe29867d2cb4
Robbie Lamb <robbie.lamb@gmail.com>
2010-08-03 12:26:50 -0600

    Namespacing project to be yogo/project

--- config/routes.rb
+++ config/routes.rb
@@ -8,43 +8,29 @@
 #
 ActionController::Routing::Routes.draw do |map|
   # The priority is based upon order of creation: first created -> highest priority.
-  map.resources :projects, :controller => 'yogo/projects',
-                :member => { :upload => :post }, 
-                :collection => { :loadexample => :post, :search => :get} do |project|
+  map.namespace :yogo do |yogo|
+    yogo.resources :projects,
+                  :member => { :upload => :post }, 
+                  :collection => { :loadexample => :post, :search => :get} do |project|
     
-    # /projects/:project_id/yogo_data/:model_name
-    # /projects/:project_id/yogo_data/:model_name/:id
-    project.resources :yogo_data, :as => 'yogo_data/:model_id', :controller => 'yogo/data',
-                      :collection => { :upload => :post, :search => :get },
-                      :member => { :download_asset => :get, :show_asset => :get }
+      # /projects/:project_id/yogo_data/:model_name
+      # /projects/:project_id/yogo_data/:model_name/:id
+      project.resources :yogo_data, :as => 'yogo_data/:model_id', :controller => 'yogo/data',
+                        :collection => { :upload => :post, :search => :get },
+                        :member => { :download_asset => :get, :show_asset => :get }
                           
-    # /projects/:project_id/yogo_models/:model_name
-    project.resources :yogo_models, :controller => 'yogo/models'
+      # /projects/:project_id/yogo_models/:model_name
+      project.resources :yogo_models, :controller => 'yogo/models'
                       
-    project.resources :users, :controller => 'yogo/users', 
-                              :only => [:index, :new, :create], 
-                              :collection => { :update_user_groups => :post }
-    
+      project.resources :users, :controller => 'yogo/users', 
+                                :only => [:index, :new, :create], 
+                                :collection => { :update_user_groups => :post }
+    end
   end
   map.resources :settings
   map.resources :tutorial
 
   map.dashboard "/dashboard", :controller => 'yogo/projects', :action => 'index'
-
-  # map.connect "/mockup/:action", :controller => 'mockup'
-  
-  # Wizard stuff
-  map.start_wizard  "/project_wizard/name", 
-                    :controller => 'project_wizard', :action => 'name'
-  map.create_wizard_project "/project_wizard/", 
-                            :controller => 'project_wizard', :action => 'create'
-  map.csv_question  "/project_wizard/csv_question/:id", 
-                    :controller => 'project_wizard', :action => 'csv_question'
-  map.import_csv  "/project_wizard/import_csv/:id", 
-                  :controller => 'project_wizard', :action => 'import_csv'
-  map.upload_csv_wizard "/project_wizard/upload_csv/:id", 
-                        :controller => 'project_wizard', :action => 'upload_csv'
-  
 
   map.resource :password, :only => [ :show, :update, :edit ]
   map.resources :users
