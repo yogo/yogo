477e1d4774a21838cd512422ca688867504b14af
Robbie Lamb <robbie.lamb@gmail.com>
2010-08-04 12:42:45 -0600

    Cherry picking lib/permission.rb from voeis.

--- lib/yogo/project_ext.rb
+++ lib/yogo/project_ext.rb
@@ -2,8 +2,16 @@ require 'yogo/project'
 
 module Yogo
   class Project
+    extend Permission
+    
     property :is_private,      Boolean, :required => true, :default => false
     
     has n, :roles
+    
+    def self.extended_permissions
+      collection_perms = [ :create_models, :retrieve_models, :update_models, :delete_models, :create_data, :retrieve_data, :update_data, :delete_data ]
+      [:manage_users, collection_perms, super].flatten
+    end
+    
   end
 end
