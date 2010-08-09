7f24fda6d13c09ff07e85dfa82d653e322159452
irjudson <irjudson@gmail.com>
2010-08-09 15:13:47 -0600

    New membership controller

--- app/controllers/yogo/members_controller.rb
+++
@@ -1,9 +0,0 @@
-class Yogo::MembersController < ApplicationController
-  inherit_resources
-
-  defaults :resource_class => Membership, :collection_name => 'memberships', :instance_name => 'membership'
-
-  belongs_to :project, :finder => :get, :parent_class => Project
-
-  respond_to :html, :json
-end
