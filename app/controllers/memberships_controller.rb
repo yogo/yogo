7029a18a556bba11fed48862dee2fa5cabb9f6af
irjudson <irjudson@gmail.com>
2010-08-10 09:15:06 -0600

    removed debugging puts.

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -12,11 +12,8 @@ class MembershipsController < ApplicationController
     @users = params[:memberships][:users].values.map { |idx| User.get(idx) }
     @roles = params[:memberships][:roles].values.map { |idx| Role.get(idx) }
 
-    puts "PARAMS: #{params.inspect}"
-
     @users.each do |u|
      @roles.each do |r|
-       puts "Adding #{u} as #{r} to #{@project}"
        Membership.create(:user => u, :role => r, :project => @project)
      end
     end
