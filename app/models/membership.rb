439cc4a4939ea5900a0ce9d1f463182b03066bdb
irjudson <irjudson@gmail.com>
2010-08-11 14:54:19 -0600

    updated membership model with UUID instead of integer id.

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -1,11 +1,11 @@
 class Membership
   include DataMapper::Resource
 
-  property :project_id, Integer, :key => true
+  property :project_id, UUID, :key => true
   property :role_id,    Integer, :key => true
   property :user_id,    Integer, :key => true
 
-  belongs_to :project
+  belongs_to :project, :model => 'Yogo::Project'
   belongs_to :role
   belongs_to :user
 end
