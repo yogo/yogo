ef6b5e26124e2821e706060f3681d2dd1b2a37f8
Robbie Lamb <robbie.lamb@gmail.com>
2010-08-12 09:31:57 -0600

    Creating roles and an initial user.

--- config/initializers/yogo.rb
+++ config/initializers/yogo.rb
@@ -11,7 +11,17 @@ require 'exceptions'
 # Load the Application Version
 load Rails.root / "VERSION"
 
+sr = SystemRole.first_or_create(:name => 'Administrator', :description => 'System role for Administrators',
+                                :permissions => User.to_permissions + Yogo::Project.to_permissions)
+
 # Create the system administrator user
 User.create(:login => 'yogo', :first_name => "System",
-            :last_name => "Administrator", :admin => true,
-            :password => 'change me', :password_confirmation => 'change me')
+            :last_name => "Administrator", :system_role => sr,
+            :password => 'change me', :password_confirmation => 'change me') if User.find_by_login('yogo').nil?
+            
+# VOEIS Specific Role Initialization
+Role.first_or_create(:name => "Principal Investigator", :description => "Principal Investigators create projects to pursue research goals.")
+Role.first_or_create(:name => "Field Technician",       :description => "Field Technicians manage field activities.")
+Role.first_or_create(:name => "Laboratory Technician",  :description => "Lab Technicians manage lab activities.")
+Role.first_or_create(:name => "Data Manager",           :description => "Data Managers manage all the data for a project.")
+Role.first_or_create(:name => "Member",                 :description => "General members of projects.")
