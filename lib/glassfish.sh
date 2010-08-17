de9cc6b0dee174ba0395f69f081de72e82908894
irjudson <irjudson@gmail.com>
2010-08-17 15:19:03 -0600

    updated kill

--- lib/glassfish.sh
+++ lib/glassfish.sh
@@ -19,7 +19,7 @@ case "$1" in
   stop)
     echo "Stopping glassfish"
     cd $RAILS_APP
-    kill -s2 `cat $RAILS_APP/../shared/pids/glassfish.pid`
+    kill -s 2 `cat $RAILS_APP/../shared/pids/glassfish.pid`
 
   ;;
 
