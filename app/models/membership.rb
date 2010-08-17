44c2f9f658e65c9a404ce36367c4cb090d362a3f
Robbie Lamb <robbie.lamb@gmail.com>
2010-08-17 16:36:28 -0600

    Updates to the project_ext Updating memberships to have permissions

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -1,7 +1,19 @@
 class Membership
   include DataMapper::Resource
+  include Facet::DataMapper::Resource
+  
+  property :project_id, UUID, :key => true
 
-  belongs_to :project, :key => true, :model => 'Yogo::Project'
+  belongs_to :project, :child_key => [:project_id], :parent_key => [:id], :model => 'Yogo::Project'
   belongs_to :role,    :key => true
   belongs_to :user,    :key => true
+  
+  def permissions_for(user)
+    (super << "#{permission_base_name}$retrieve").uniq
+  end
+  
+  def permissions_for(user)
+    # return ["#{permission_base_name}$retrieve"] if user.nil?
+    super + user.memberships(:project_id => project.id).roles.map{|r| r.actions }.flatten.uniq
+  end
 end
--- lib/yogo/project_ext.rb
+++ lib/yogo/project_ext.rb
@@ -2,35 +2,26 @@ require 'yogo/project'
 
 module Yogo
   class Project
-    extend Permission
+    # extend Permission
     include Facet::DataMapper::Resource
     
     property :is_private,      Boolean, :required => true, :default => false
     
-    has n, :memberships
+    has n, :memberships, :parent_key => [:id], :child_key => [:project_id], :model => 'Membership'
     has n, :roles, :through => :memberships
     has n, :users, :through => :memberships
     
+    after :create, :give_current_user_membership
     before :destroy, :destroy_cleanup
     
-    def self.extended_permissions
-      collection_perms = ['collection', 'item'].map do |elem|
-        self.basic_permissions.map{|p| "#{p}_#{elem}".to_sym }
-      end
-      
-      # collection_perms = [ :create_collection, :retrieve_collection, :update_collection, :delete_collection, :create_data, :retrieve_data, :update_data, :delete_data ]
-      [:manage_users, collection_perms, super].flatten
-    end
-    
-    ##
-    # @return [Boolean]
-    # @api public
-    def allow_access?(permission, user)
-      # we want all admins to access this resource
-      return true if user.admin?
-      user.roles(:project => self).any?{|r| r.has_permission?(permission) }
-    end
-    
+    # def self.extended_permissions
+    #   collection_perms = ['collection', 'item'].map do |elem|
+    #     self.basic_permissions.map{|p| "#{p}_#{elem}".to_sym }
+    #   end
+    #   
+    #   # collection_perms = [ :create_collection, :retrieve_collection, :update_collection, :delete_collection, :create_data, :retrieve_data, :update_data, :delete_data ]
+    #   [:manage_users, collection_perms, super].flatten
+    # end
     
     def self.permissions_for(user)
       (super << "#{permission_base_name}$retrieve").uniq
@@ -40,13 +31,19 @@ module Yogo
     # 
     def permissions_for(user)
       return ["#{permission_base_name}$retrieve"] if user.nil?
-      super + user.memberships(:project_id => self.id).roles.map{|r| r.permissions}.flatten.uniq
+      super + user.memberships(:project_id => self.id).roles.map{|r| r.actions }.flatten.uniq
     end
     
     private
     
     def destroy_cleanup
       memberships.destroy
+    end
+    
+    def give_current_user_membership
+      unless User.current.nil?
+        Membership.create(:user => User.current, :project => self, :role => Role.first(:position => 1))
+      end
     end
 
   end
