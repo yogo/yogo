5cd207a95525f15634d6bf591832a4201529e646
Robbie Lamb <robbie.lamb@gmail.com>
2010-08-18 10:33:24 -0600

    Updating system_roles_controller to be able to redirect properly

--- app/controllers/system_roles_controller.rb
+++ app/controllers/system_roles_controller.rb
@@ -1,4 +1,4 @@
-class UsersController < InheritedResources::Base
+class SystemRolesController < InheritedResources::Base
   
   respond_to :html, :json
   
@@ -6,14 +6,36 @@ class UsersController < InheritedResources::Base
            :collection_name => 'system_roles',
            :instance_name => 'system_role'
            
+   def create
+     create! do |format|
+       format.html { redirect_to(system_roles_url) }
+     end
+   end
+
+   def update
+     update! do |format|
+       format.html { redirect_to(system_roles_url) }
+     end
+   end
+
+   def delete
+     delete! do |format|
+       format.html { redirect_to(system_roles_url) }
+     end
+   end
+           
    protected
 
    def collection
-     @roles ||= resource_class.all
+     @system_roles ||= resource_class.all
    end
 
    def resource
-     @role ||= resource_class.get(params[:id])
+     @system_role ||= resource_class.get(params[:id])
+   end
+   
+   def resource_class
+     SystemRole.access_as(current_user)
    end
            
 end
