8cbcb1579f32151fd8cecb78bb3328bdbf6580f3
Ryan Heimbuch <rheimbuch@gmail.com>
2010-08-18 18:22:30 -0600

    Adding InheritedResources overrides to ProjectsController.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,17 @@
 class ProjectsController < InheritedResources::Base
   respond_to :html, :json
   
+  protected
+  
+  def resource
+    @project ||= collection.get(params[:id])
+  end
+
+  def collection
+    @projects ||= resource_class.all
+  end
+
+  def resource_class
+    Project
+  end
 end
