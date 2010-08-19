0855967b6db85066fbe8215cef800f8416e10156
Robbie Lamb <robbie.lamb@gmail.com>
2010-08-19 12:44:58 -0600

    Filting parameter password_confirmation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -31,7 +31,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery 
 
   # Scrub sensitive parameters from your log
-  filter_parameter_logging :password
+  filter_parameter_logging :password, :password_confirmation
   
   rescue_from Facet::PermissionException::Denied, :with => :authorization_denied
   # rescue_from Facet::PermissionException::Denied, :with => :authentication_required
