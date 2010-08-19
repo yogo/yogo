4afc919e4b4461b8c19bb4c38b9563c728cd3621
Robbie Lamb <robbie.lamb@gmail.com>
2010-08-19 13:08:15 -0600

    Removing permissions_for on memembership
    Should be passed up to the root object of the query.

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -8,12 +8,4 @@ class Membership
   belongs_to :role,    :key => true
   belongs_to :user,    :key => true
   
-  def permissions_for(user)
-    (super << "#{permission_base_name}$retrieve").uniq
-  end
-  
-  def permissions_for(user)
-    # return ["#{permission_base_name}$retrieve"] if user.nil?
-    super + user.memberships(:project_id => project.id).roles.map{|r| r.actions }.flatten.uniq
-  end
 end
