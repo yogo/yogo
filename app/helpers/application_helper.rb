7b3f06d5f3360d52d43cf8756486bbd44bca8757
irjudson <irjudson@gmail.com>
2010-08-24 08:34:06 -0600

    Updating views (tweaking).

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -61,7 +61,7 @@ module ApplicationHelper
     if (from == 1 and to == collection.count)
       output = ""
     else
-      output += " <span style='font-style: italic'>Showing #{from}&hellip;#{to} rows out of #{collection.count} total rows</span>"
+      output += " <span style='font-style: italic'>Showing #{from}&hellip;#{to} #{collection.name} (#{collection.count} total)</span>"
     end
   end
 
@@ -116,6 +116,14 @@ module ApplicationHelper
     "<br clear='all' style='clear: both;'/>"
   end
 
+  def page_title
+    # debugger
+    if params.has_key?(:id) && resource.respond_to?(:name)
+      "#{controller_name.capitalize} >> #{resource.name}"
+    else
+      controller_name.capitalize
+    end
+  end
 
   ##
   # Helper for creating a tooltip
