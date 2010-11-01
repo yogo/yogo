1bc5fceb9442a48e3cdfc64a2ac73c075a8a8914
irjudson <irjudson@gmail.com>
2010-11-01 14:33:54 -0600

    Interim.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yogo::Application.config.secret_token = '8931d9d66b87e0ca98e36ccd0e4bcf68988e6510e1a8e336eb7ffb175351a05e2071299b76b8615d72f13de824f20c4e861caddfeb9bfdfc4c50bbd62c5a36f1'
