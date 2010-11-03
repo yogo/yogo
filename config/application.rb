bc5095262a33e98687a966d0b6d23580013a0b2f
Robbie Lamb <robbie.lamb@gmail.com>
2010-11-03 13:49:55 -0600

    Filtering sensitive parameters.

--- config/application.rb
+++ config/application.rb
@@ -38,5 +38,7 @@ module Yogo
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
     # config.i18n.default_locale = :de
+    
+    config.filter_parameters = [:password, :password_confirmation]
   end
 end
