234563fc3d5e23c3067c089f06c46217ccbcc9cf
Robbie Lamb <robbie.lamb@gmail.com>
2010-11-04 08:55:15 -0600

    Don't use database before initialization.

--- config/initializers/yogo.rb
+++ config/initializers/yogo.rb
@@ -7,6 +7,6 @@ load File.join(::Rails.root.to_s, "VERSION")
 ##
 # Check to make sure the asset directory exists and make it if it doesn't
 # TODO: If the user changes the setting we should make sure that directory exists...but we don't.
-if Setting['asset_directory'] && ! File.directory?(File.join(::Rails.root.to_s, Setting['asset_directory']))
-  FileUtils.mkdir_p(File.join(::Rails.root.to_s, Setting['asset_directory']))
-end
+# if Setting['asset_directory'] && ! File.directory?(File.join(::Rails.root.to_s, Setting['asset_directory']))
+#   FileUtils.mkdir_p(File.join(::Rails.root.to_s, Setting['asset_directory']))
+# end
