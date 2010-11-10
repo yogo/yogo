3237f1e20357f80f0df8250a2bc72ac38ba2f447
irjudson <irjudson@gmail.com>
2010-11-10 12:14:32 -0700

    Fixed version loading in managed repose

--- config/initializers/datamapper.rb
+++ config/initializers/datamapper.rb
@@ -20,41 +20,32 @@ DataMapper::Model.raise_on_save_failure = true
 # Read the configuration from the existing database.yml file
 # config = Rails.configuration.database_configuration
 
-
 # Load the project model and migrate it if needed.
-#Project
-# Setting
-# User
-# Role
-# Membership
-# Site
-# Unit
-# Variable
-# FieldMethod
-# VariableNameCV
-# SampleMediumCV
-# ValueTypeCV
-# SpeciationCV
-# DataTypeCV
-# GeneralCategoryCV
-# SampleTypeCV
-# SampleMaterial
-# LabMethod
+Project
+Project
+Setting
+User
+Role
+Membership
+Site
+Unit
+Variable
+FieldMethod
+VariableNameCV
+SampleMediumCV
+ValueTypeCV
+SpeciationCV
+DataTypeCV
+GeneralCategoryCV
+SampleTypeCV
+SampleMaterial
+LabMethod
 
-# DataMapper.finalize
-# DataMapper.auto_migrate! unless DataMapper.repository(:default).storage_exists?(Project.storage_name) &&
-# DataMapper.repository(:default).storage_exists?(Setting.storage_name) &&
-# DataMapper.repository(:default).storage_exists?(User.storage_name) &&
-# DataMapper.repository(:default).storage_exists?(Role.storage_name) &&
-# DataMapper.repository(:default).storage_exists?(Membership.storage_name) &&
-# DataMapper.repository(:default).storage_exists?(Site.storage_name) &&
-# DataMapper.repository(:default).storage_exists?(Unit.storage_name) &&                         DataMapper.repository(:default).storage_exists?(Variable.storage_name) &&
-# DataMapper.repository(:default).storage_exists?(FieldMethod.storage_name) &&                      DataMapper.repository(:default).storage_exists?(VariableNameCV.storage_name) &&
-# DataMapper.repository(:default).storage_exists?(SampleMediumCV.storage_name) &&
-# DataMapper.repository(:default).storage_exists?(ValueTypeCV.storage_name) &&
-# DataMapper.repository(:default).storage_exists?(SpeciationCV.storage_name) &&
-# DataMapper.repository(:default).storage_exists?(DataTypeCV.storage_name) &&
-# DataMapper.repository(:default).storage_exists?(GeneralCategoryCV.storage_name) &&
-# DataMapper.repository(:default).storage_exists?(SampleTypeCV.storage_name) &&
-# DataMapper.repository(:default).storage_exists?(SampleMaterial.storage_name) &&
-# DataMapper.repository(:default).storage_exists?(LabMethod.storage_name)
+DataMapper.finalize
+
+DataMapper::Model.descendants.each do |model|
+  begin
+    model::Version
+  rescue
+  end
+end
