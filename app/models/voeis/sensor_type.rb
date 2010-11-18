05c5d6c0e3f808ba43af51604e8dd11e4958f6dd
Robbie Lamb <robbie.lamb@gmail.com>
2010-11-18 15:51:37 -0700

    More munging of models.

--- app/models/voeis/sensor_type.rb
+++ app/models/voeis/sensor_type.rb
@@ -6,24 +6,20 @@ class Voeis::SensorType
   include DataMapper::Resource
   include Facet::DataMapper::Resource
 
-  property :id, Serial
-  property :name,     String,  :required => true, :length => 512
-  property :min,      Float,  :required => false
-  property :max,      Float,  :required => false
+  property :id,          Serial
+  property :name,        String,  :required => true, :length => 512
+  property :min,         Float,  :required => false
+  property :max,         Float,  :required => false
   property :difference,  Float,  :required => false
-  property :updated_at, DateTime, :required => true,  :default => DateTime.now
+
+  timestamps :at
 
   is_versioned :on => :updated_at
-  
-  before(:save) {
-    self.updated_at = DateTime.now
-  }
 
   has n, :sites,          :model => "Voeis::Site", :through => Resource
   has n, :data_stream_columns, :model => 'Voeis::DataStreamColumn', :through => Resource
   has n, :sensor_type_sensor_values, :model => 'Voeis::SensorTypeSensorValue'
   has n, :sensor_values,   :through => :sensor_type_sensor_value
   has n, :variables,      :model => "Voeis::Variable", :through => Resource
-  #has n, :methods, :model => "Method", :through => Resource
 
 end
