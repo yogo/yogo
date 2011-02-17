b596d758d7048be96c4edddc53d141182c41257a
Sean Cleveland <sean.b.cleveland@gmail.com>
2011-02-17 13:31:22 -0700

    Added a single data measurement input view for sensor value

--- app/controllers/voeis/sensor_values_controller.rb
+++ app/controllers/voeis/sensor_values_controller.rb
@@ -8,4 +8,9 @@ class Voeis::SensorValuesController < Voeis::BaseController
             :instance_name => 'sensor_value',
             :resource_class => Voeis::SensorValue
   
+  def new
+      @sites = parent.managed_repository{Voeis::Site.all}
+      @variables = Variable.all
+      @units = Unit.all
+  end
 end
