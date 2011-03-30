5b0ac80a1b9a209b1f17d3820f1d7bd6be975402
Ryan Heimbuch <rheimbuch@gmail.com>
2011-03-30 10:39:53 -0600

    Fix project_menu_cell

--- app/cells/project_menu_cell.rb
+++ app/cells/project_menu_cell.rb
@@ -1,11 +1,8 @@
 class ProjectMenuCell < Cell::Rails
 
   def display
-    current_user = @opts[:current_user]
-    @projects = Project.all(:is_private => false)
-    if @opts[:logged_in]
-      @projects = @projects | current_user.projects
-    end
+    @current_user = @opts[:current_user]
+    @projects = @current_user.projects
     render
   end
 
