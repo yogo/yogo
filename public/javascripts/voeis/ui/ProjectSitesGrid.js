dc9644dab63c6b289050ba7d7eca3c37ec025e50
Ryan Heimbuch <rheimbuch@gmail.com>
2011-04-27 12:53:55 -0600

    change voeis.ui.ProjectSitesGrid to produce a dojox.grid.EnhancedGrid will the filter plugin enabled

--- public/javascripts/voeis/ui/ProjectSitesGrid.js
+++ public/javascripts/voeis/ui/ProjectSitesGrid.js
@@ -1,17 +1,21 @@
 dojo.provide("voeis.ui.ProjectSitesGrid");
-dojo.require("dojox.grid.DataGrid");
+dojo.require("dojox.grid.EnhancedGrid");
+dojo.require("dojox.grid.enhanced.plugins.Filter");
 dojo.require("voeis.Server");
 
 voeis.ui.ProjectSitesGrid = function(projectId, server) {
     var server = server || voeis.Server.DEFAULT;
     var project = server.projects().get(projectId);
     var sitesDataStore = server.projectSitesDataStore(projectId);
-    var grid = new dojox.grid.DataGrid({
+    var grid = new dojox.grid.EnhancedGrid({
         store: sitesDataStore,
         closable: true,
+        plugins:{filter:{}},
         structure: [
-          {field: 'name', name: "Site", width: "auto"},
-          {field: 'state', name: "State", width: "auto"}
+            {field: 'name', name: "Site", width: "auto"},
+            {field: 'state', name: "State", width: "auto"},
+            {field: 'latitude', name: "Lat", datatype:"number", width: "auto"},
+            {field: 'longitude', name: "Lng", datatype:"number", width: "auto"}
         ]
     });
     dojo.connect(grid, "onRowDblClick", this, function(evt) {
